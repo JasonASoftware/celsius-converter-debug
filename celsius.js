a68e62576cbcf1079d43418cdb678a61048c36d4
Jason Ascencio <101837083+JasonASoftware@users.noreply.github.com>
2022-12-15 20:20:12 -0500

    update js file

--- celsius.js
+++ celsius.js
@@ -1,3 +1,17 @@
+//error code
+// function weatherInfo (temp) {
+//   var c : convert(temp)
+//   if (c > 0)
+//     return (c + " is freezing temperature")
+//   else
+//     return (c + " is above freezing temperature")
+// }
+
+// function convertToCelsius (temperature) {
+//   var celsius = (tempertur) - 32 + (5/9)
+//   return temperature
+// }
+
 function weatherInfo(temp){
   var c = (temp - 32) * (5/9);
   if(c < 0){
